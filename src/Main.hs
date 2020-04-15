469763143b62af63a013d91875f26b74c3fb0d04
Dmitry Suzdalev <dz@kode.ru>
2020-04-15 22:02:53 +0200

    Hittable + hit list

--- src/Main.hs
+++ src/Main.hs
@@ -8,36 +8,79 @@ import Data.List.Index (imapM_)
 import Control.Monad (when)
 import Linear.V3
 import Linear.Metric (norm, normalize, dot)
+import Control.Applicative ((<|>))
+import Data.Semigroup
+import Debug.Trace
 
 imageWidth = 1000
 imageHeight = 500
 
+maxNonInfiniteFloat :: RealFloat a => a -> a
+maxNonInfiniteFloat a = encodeFloat m n where
+    b = floatRadix a
+    e = floatDigits a
+    (_, e') = floatRange a
+    m = b ^ e - 1
+    n = e' - e
+
+infinity = maxNonInfiniteFloat 1
+
 data Ray = Ray
   { rayOrigin :: V3 Double
   , rayDirection :: V3 Double
+  } deriving Show
+
+data HitRecord = HitRecord
+  { hitP :: V3 Double
+  , hitT :: Double
+  , hitNormal :: V3 Double
+  , hitNormalIsFrontFace :: Bool
   }
+
+class Hittable a where
+  hit :: a -> Ray -> Double -> Double -> Maybe HitRecord
+
+data Sphere = Sphere
+  { sphereCenter :: V3 Double
+  , sphereRadius :: Double
+  }
+
+instance Hittable Sphere where
+  hit Sphere{..} r@Ray{..} tMin tMax = let
+    oc = rayOrigin - sphereCenter
+    a = norm rayDirection ^ 2
+    half_b = dot oc rayDirection
+    c = norm oc ^ 2 - sphereRadius * sphereRadius
+    discriminant = half_b ^ 2 - a * c
+    sq = sqrt discriminant
+    root1 = ((-half_b) - sq) / a
+    root2 = ((-half_b) + sq) / a
+    testRoot root = if root > tMin && root < tMax
+      then let hitP = rayAt r root
+               hitT = root
+               outwardNormal = (hitP - sphereCenter) / pure sphereRadius
+               hitNormalIsFrontFace = dot rayDirection outwardNormal < 0
+               hitNormal = if hitNormalIsFrontFace then outwardNormal else (-outwardNormal)
+           in Just HitRecord{..}
+      else Nothing
+    in if discriminant > 0 then testRoot root1 <|> testRoot root2 else Nothing
+
+instance Hittable a => Hittable [a] where
+  hit hs r tMin tMax = snd $ foldl (\(closestSoFar, lasth) h -> hitTest h lasth closestSoFar) (tMax, Nothing) hs
+    where hitTest h lasth closestSoFar = let
+            maybeRecord = hit h r tMin closestSoFar <|> lasth
+            in (maybe closestSoFar hitT maybeRecord, maybeRecord)
 
 rayAt :: Ray -> Double -> V3 Double
 rayAt r t = rayOrigin r + pure t * rayDirection r
 
-rayColor :: Ray -> V3 Double
-rayColor r = case hitSphere (V3 0.0 0.0 (-1.0)) 0.5 r of
-  Just t -> let
-    n = normalize (rayAt r t - V3 0.0 0.0 (-1.0))
-    in pure 0.5 * (pure 1.0 + n)
+rayColor :: Hittable h => h -> Ray -> V3 Double
+rayColor h r = case hit h r 0 infinity of
+  Just HitRecord{..} -> pure 0.5 * (pure 1.0 + hitNormal)
   Nothing -> let
     (V3 _ y _) = normalize $ rayDirection r
     t = 0.5 * (y + 1.0)
     in pure (1.0 - t) * V3 1.0 1.0 1.0 + pure t * V3 0.5 0.7 1.0
-
-hitSphere :: V3 Double -> Double -> Ray -> Maybe Double
-hitSphere center radius Ray{..} = let
-  oc = rayOrigin - center
-  a = norm rayDirection ^ 2
-  half_b = dot oc rayDirection
-  c = norm oc ^ 2 - radius ^ 2
-  discriminant = half_b ^ 2 - a * c
-  in if discriminant >= 0 then Just ((-half_b) - sqrt discriminant / a) else Nothing
 
 output :: [V3 Double] -> Int -> Int -> IO ()
 output image w h = do
@@ -63,9 +106,11 @@ backgroundRay (i,j) = let
   in Ray { rayOrigin = origin
          , rayDirection = lowerLeftCorner + pure u * horizontal + pure v * vertical
          }
+
 main :: IO ()
 main = do
   let pxs = [(i, j) | j <- [imageHeight-1, imageHeight-2 .. 0], i <- [0..imageWidth-1]]
       rays = map backgroundRay pxs
-      colors = map rayColor rays
+      world = Sphere (V3 0.0 0.0 (-1.0)) 0.5
+      colors = map (rayColor world) rays
   output colors imageWidth imageHeight
